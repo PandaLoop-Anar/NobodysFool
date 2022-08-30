141e7c102aeb15333a63fe19d64bde3850ee1380
Anar Mamedovi <84316960+PandaLoop-Anar@users.noreply.github.com>
2022-08-30 22:47:23 +0400

    Create aboutUs.data.js

---
+++ data/aboutUs.data.js
@@ -0,0 +1,6 @@
+export const aboutUsData = {
+  img_url: "",
+  img_text: "215 X 90 px",
+  text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nunc non diam erat. In Fringilla massa ut nissi ullamocorper pellentesque. Quisque non luctus sem. Nullam non magna vel nisi posuere bibendum vitae sed dui.",
+  btn_link: "#",
+};
